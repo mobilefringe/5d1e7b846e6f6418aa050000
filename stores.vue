992462523717de1a226c1c8cd8925c6995777975
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-05 11:04:26 -0400

    Kanata Central BIA 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -11,7 +11,7 @@
                 </div>
 				<div class="col-sm-6 col-md-4">
 					<div class="store_search" >
-						<search-component :list="allStores" :placeholder="$t('stores_page.find_your_store')" suggestion-attribute="name" v-model="search_result" @select="onOptionSelect" class="text-left" :keys="['name']" :tokenize="true">
+						<search-component :list="allStores" placeholder="Find Your Store" suggestion-attribute="name" v-model="search_result" @select="onOptionSelect" class="text-left" :keys="['name']" :tokenize="true">
 							<template slot="item" scope="option" class="manual">
 								<article class="media">
 									<p>
