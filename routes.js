82fb36d8dfe433f0c4ed1053c378aab9c2f8cfb4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-09 14:42:24 -0400

    Kanata Central BIA 1.0  /routes.js

---
+++ routes.js
@@ -0,0 +1,146 @@
+define([], function() {
+    return [{
+            path: '/',
+            component: view('home')
+        },
+        {
+            path: '/stores',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('stores'),
+                    name: 'storeList'
+                },
+                {
+                    path: ':id',
+                    component: view('store_details'),
+                    name: 'storeDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/promotions',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('promotions'),
+                    name: 'promoList'
+                },
+                {
+                    path: ':id',
+                    component: view('promotion_details'),
+                    name: 'promotionDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/events',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('events'),
+                    name: 'eventList'
+                },
+                {
+                    path: ':id',
+                    component: view('event_details'),
+                    name: 'eventDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/jobs',
+            component: view('default'),
+            children: [{
+                    path: '',
+                    component: view('jobs'),
+                    name: 'jobList'
+                },
+                {
+                    path: ':id',
+                    component: view('job_details'),
+                    name: 'jobDetails',
+                    props: true
+                }
+            ]
+        },
+        {
+            path: '/map',
+            component: view('default'),
+            redirect: '/404',
+            children: [{
+                path: '',
+                component: view('map'),
+                name: 'map'
+            }]
+        },
+        {
+            path: '/hours',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('hours'),
+                name: 'hourList'
+            }]
+        },
+        {
+            path: '/contest',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('contest'),
+                name: 'contestList'
+            }]
+        },
+        {
+            path: '/contact-us',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('contact_us'),
+                name: 'contactUs'
+            }]
+        },
+        {
+            path: '/newsletter',
+            component: view('default'),
+            children: [{
+                path: '',
+                component: view('newsletter'),
+                name: 'newsletter'
+            }]
+        },
+        {
+            path: '/pages',
+            component: view('default'),
+            redirect: '/404',
+            children: [{
+                path: ':id',
+                component: view('page_details'),
+                name: 'pageDetails'
+            }]
+        },
+        {
+            path: '/404',
+            name: '404',
+            component: view('notfoundcomponent')
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ]
+
+    /**
+     * Asynchronously load view (lazy-loading)
+     * @param {string} name the filename (basename) of the view to load.
+     */
+    function view(name) {
+        return function(resolve) {
+            require(['vue!' + name + '.vue'], resolve);
+        }
+    };
+});
