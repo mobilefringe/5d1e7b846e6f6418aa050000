42f1958cbee28773f2cd6d607eca88dbdcee0a5c
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-10 15:08:59 -0400

    Kanata Central BIA 1.0  /welcome_msg.vue

--- welcome_msg.vue
+++ welcome_msg.vue
@@ -1,8 +1,8 @@
 <template>
-    <div v-if="currentMessage" class="main_container">
+    <div v-if="property" class="main_container">
         <div class="message_content">
-            <h1 class="center">{{ currentMessage.title }}</h1>
-            <p class="center">{{ currentMessage.body }}</p> 
+            <h1 class="center">{{ property.welcome_message }}</h1>
+            <p class="center">{{ property.description }}</p> 
         </div>
     </div>
 </template>
@@ -11,35 +11,10 @@
     define(["Vue", "vuex"], function(Vue, Vuex){
         return Vue.component("messages-component", {
             template: template, // the variable template will be injected
-            data: function() {
-                return {
-                    currentMessage: null
-                }
-            },
-            created(){
-                this.loadData().then(response => {
-                    if(response[0].data.messages.welcome[0] != undefined){
-                        this.currentMessage = response[0].data.messages.welcome[0].messages[0];
-                    } else {
-                        console.log("No message at this time.");       
-                    }
-                });
-            },
             computed: {
                 ...Vuex.mapGetters([
                     'property'
                 ])
-            },
-            methods: {
-                loadData: async function() {
-                    this.property.mm_host = this.property.mm_host.replace("http:", "");
-                    try {
-                        let results = await Promise.all([this.$store.dispatch('LOAD_PAGE_DATA', {url:this.property.mm_host + "/api/v3/cerritos/messages.json"})]);
-                        return results;
-                    } catch(e) {
-                        console.log("Error loading data: " + e.message);    
-                    }
-                }
             }
         })
     });
