ebb14bcc1fec6e752cbb612d18c24bc25558ae56
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-14 18:12:41 -0400

    Kanata Central BIA 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -28,7 +28,9 @@
                             :on-change="dropPin">
                         </v-select>
                     </div>
-                    <mapplic-map ref="mapplic_ref" :height="700" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="mapStores" :floorlist="floorList" :svgWidth="2500" :svgHeight="2500" tooltiplabel="View Details"></mapplic-map>
+                    <mapplic-map v-if="!isMobile" ref="mapplic_ref" :height="700" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="mapStores" :floorlist="floorList" :svgWidth="2500" :svgHeight="2500" tooltiplabel="View Details"></mapplic-map>
+                    
+                    <mapplic-map v-if="isMobile" ref="mapplic_ref" :height="350" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="mapStores" :floorlist="floorList" :svgWidth="2500" :svgHeight="2500" tooltiplabel="View Details"></mapplic-map>
                 </div>
             </div>
             <div class="padding_top_40"></div>
