c6be6a1bc6aceb9f3226755f7c5a6c09cbbe283f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-14 16:27:23 -0400

    Kanata Central BIA 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -35,41 +35,119 @@
 </style>
 
 <script>
-    define(["Vue","vuex", "vue!mapplic-map"], function(Vue, Vuex, MapplicComponent) {
-        return Vue.component("map-component", {
+    define(["Vue", "vuex", "vue-select", "vue!mapplic-map"], function(Vue, Vuex, VueSelect, MapplicComponent) {
+        return Vue.component("stores-component", {
             template: template, // the variable template will be injected
-            
+            data: function() {
+                return {
+                    listMode: "alphabetical",
+                    selectedCat: null,
+                    selectedAlpha: "All",
+                    alphabet: ["All", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+                    filteredStores: null,
+                    dataloaded: false,
+                    mobile_store: false,
+                    windowWidth: 0,
+                    storeBanner : null,
+                    search_result : null,
+                }
+            },
+            created (){
+                this.loadData().then(response => {
+                    this.dataloaded = true;
+                    this.filteredStores = this.allStores;
+
+                    var temp_repo = this.findRepoByName('Map Banner');
+                    if(temp_repo && temp_repo.images) {
+                        this.pageBanner = temp_repo.images[0];
+                    }
+                    else {
+                        this.pageBanner = {};
+                        this.pageBanner.image_url = "";
+                    }
+                });
+            },
+            watch: {
+                windowWidth: function() {
+                    if (this.windowWidth <= 768) {
+                        this.mobile_store = true;
+                    } else {
+                        this.mobile_store = false;
+                    }
+                },
+            },
+            mounted() {
+                this.$nextTick(function() {
+                    window.addEventListener('resize', this.getWindowWidth);
+                    //Init
+                    this.getWindowWidth();
+                });
+            },
             computed: {
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
-                    'processedStores'
+                    'processedStores',
+                    'findRepoByName'
                 ]),
                 allStores() {
-                    return this.processedStores;
+                    var all_stores = this.processedStores;
+                    _.forEach(all_stores, function(value, key) {
+                        value.zoom = 2;
+                    });
+                    return all_stores;
                 },
-                getSVGurl () {
-                    return "https://www.mallmaverick.com" + this.property.svgmap_url; 
+                mapStores() {
+                    var all_stores = this.processedStores;
+                    _.forEach(all_stores, function(value, key) {
+                        value.zoom = 2;
+                        if(value.svgmap_region == null){
+                            value.svgmap_region = value.id;
+                        }
+                    });
+                    return all_stores;
                 },
-
+                storeNames () {
+                    return _.map(this.processedStores, 'name');
+                },
+                getSVGMap(){
+                  return "//mallmaverick.com"+this.property.svgmap_url;  
+                },
                 floorList () {
                     var floor_list = [];
-                    
                     var floor_1 = {};
                     floor_1.id = "first-floor";
                     floor_1.title = "Level One";
-                    floor_1.map = this.getSVGurl;
-                    floor_1.z_index = 1;
+                    floor_1.map = this.getSVGMap;
+                    floor_1.z_index = null;
                     floor_1.show = true;
                     floor_list.push(floor_1);
+                    
                     return floor_list;
                 }
             },
             methods: {
+                loadData: async function() {
+                    try {
+                        let results = await Promise.all([this.$store.dispatch("getData", "categories"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
                 dropPin(store) {
                     this.$refs.mapplic_ref.showLocation(store.svgmap_region);
-                }
-            }
+                },
+                getWindowWidth(event) {
+                    this.windowWidth = window.innerWidth;
+                },
+                onOptionSelect(option) {
+                    this.search_result = "";
+                    this.dropPin(option);
+                },
+            },
+            beforeDestroy: function() {
+                window.removeEventListener('resize', this.getWindowWidth);
+            },
         });
     });
 </script>
