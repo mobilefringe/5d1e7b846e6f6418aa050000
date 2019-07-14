81f7c12583aba1706ef3fa075b10ca2e9f1aa720
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-14 16:44:13 -0400

    Kanata Central BIA 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -12,9 +12,8 @@
                     <div class="select_container show_phone">
                         <v-select :options="mapStores" :placeholder="'Select A Store'" :searchable="false" :label="'name'" :on-change="dropPin" id="mobile_alpha_list"></v-select> 
                     </div>
-                    <div class="map_container">
-                        <div class="map_storelist hidden_phone">
-                        <v-select :options="mapStores" :placeholder="'Select A Store'" :searchable="false" :label="'name'" :on-change="dropPin"></v-select>                         </div>
+                    <div class="map_storelist hidden_phone">
+                        <v-select :options="mapStores" :placeholder="'Select A Store'" :searchable="false" :label="'name'" :on-change="dropPin"></v-select>
                     </div>
                     <mapplic-map ref="mapplic_ref" :height="700" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="mapStores" :floorlist="floorList" :svgWidth="2500" :svgHeight="2500" tooltiplabel="Info"></mapplic-map>
                 </div>
