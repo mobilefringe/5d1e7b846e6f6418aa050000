1cb0b89bee31bbff8e63e835b1225b8f0bcbfd57
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-14 22:38:14 -0400

    Kanata Central BIA 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -89,7 +89,7 @@
                 <div class="col-md-12 promo_item" id="promos_main" v-if="currentStore && currentStore.total_published_promos > 0">
                     <h2 class="store_details_promo_heading sub_title">{{ currentStore.name }} Promotions</h2>
                     <div id="promos_container" class="row">
-                        <div class="col-md-6 col-sm-6 no_padding" v-for="promo in promotions" :data-cat="promo.cat_list">
+                        <div class="col-md-6" v-for="promo in promotions" :data-cat="promo.cat_list">
                             <div class="promo_item cats_row is-table-row">
                                 <div class="col-md-5 col-xs-4 no_padding">
                                     <img class="promo_store_image" :src="promo.store.image_url" :alt="promo.name" />
