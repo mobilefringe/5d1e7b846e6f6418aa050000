44d85f5be61cc7da2d379b7f075b411a2c06a329
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-14 22:46:16 -0400

    Kanata Central BIA 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -97,7 +97,6 @@
                                 <div class="col-md-7 padding_tb_20">
                                     <h2 class="promo_list_name">{{ promo.name }}</h2>
                                     <p class="promo_dates sub_title">{{ promo.start_date | moment("MMM D", timezone) }} - {{ promo.end_date | moment("MMM D", timezone) }}</p>
-                                    <div class="promo_list_desc hidden_phone">{{ promo.description_short }}</div>
                                     <div class="text_center position_relative hidden_phone">
                                         <router-link :to="'/promotions/' + promo.slug" class="animated_btn text_center">View Details</router-link>
                                     </div>
