6afd7948e90ad52aeb0a19e7368b5d468f157595
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-07-24 14:32:38 -0400

    Kanata Central BIA 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -43,12 +43,11 @@
                             <img class="promo_store_image" :src="promo.store.store_front_url_abs" :alt="promo.name" />
                         </div>
                         <div class="col-md-7 padding_tb_20">
-                            <router-link :to="'/promotions/'+promo.slug" class="">
-                                <h2 class="promo_list_name">{{promo.name}}</h2>
-                            </router-link>
-                            <p>
-                                <span class="promo_dates sub_title">{{promo.start_date | moment("MMM D", timezone)}} - {{promo.end_date | moment("MMM D", timezone)}}</span>
+                            <h2 class="promo_list_name">{{promo.name}}</h2>
+                            <p class="promo_dates sub_title" v-if="isMultiDay(promo)">
+                                {{ promo.start_date | moment("MMM D", timezone) }} - {{ promo.end_date | moment("MMM D", timezone) }}
                             </p>
+                            <p class="promo_dates sub_title" v-else>{{ promo.start_date | moment("MMM D", timezone)}}</p>
                             <div class="promo_list_desc hidden_phone">{{promo.description_short }}</div>
                             <div class="text_center position_relative hidden_phone">
                                 <router-link :to="'/promotions/'+promo.slug" class="animated_btn text_center">Read More</router-link>
