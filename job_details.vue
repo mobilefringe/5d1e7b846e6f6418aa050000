605e2e216eea9d4e77169414908a13573f8c98b0
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-08-22 16:33:04 -0400

    Kanata Central BIA 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -39,7 +39,7 @@
 
 <script>
     define(["Vue", "vuex","moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
-        return Vue.component("event-details-component", {
+        return Vue.component("job-details-component", {
             template: template, // the variable template will be injected,
             props:['id'],
             data: function() {
@@ -75,9 +75,8 @@
                 ...Vuex.mapGetters([
                     'property',
                     'timezone',
-                    'processedEvents',
-                    'findEventBySlug',
-                    'findEventById'
+                    'processedJobs',
+                    'findJobBySlug'
                 ])
             },
             methods: {
