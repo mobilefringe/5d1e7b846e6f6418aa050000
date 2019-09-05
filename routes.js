7215c8e26636ca25c1c827e35822a0bd466e4feb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-09-05 15:34:16 -0400

    Kanata Central BIA 1.0  /routes.js

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ define([], function() {
                 {
                     path: '',
                     component: view('contact'),
-                    name: 'contactUs'
+                    name: 'contact'
                 }
             ]
         },
