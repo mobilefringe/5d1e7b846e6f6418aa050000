bb310729ab26f68f73a984784ab0cab99a88d058
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-09-23 13:53:37 -0400

    Kanata Central BIA 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -115,7 +115,7 @@
                     initZoom.z_coordinate = 1;
                     initZoom.x = 0.5;
                     initZoom.y = 0.5;
-                    initZoom.zoom = 1.5;
+                    initZoom.zoom = 2;
                     all_stores.push(initZoom);
         
                     return all_stores;
