0b65b1f6471756edcc3158986beb6662ad6dcb2e
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2020-03-05 17:07:49 -0500

    Kanata Central BIA 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -32,16 +32,17 @@
                 }
             },
             metaInfo() {
+                console.log(this.meta.meta_title)
                 return {
-      title: this.meta.meta_title,
-      meta: [
-         { name: 'description', vmid: 'description', content: this.meta.meta_description },
-         { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
-         { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
-         { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
-         { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
-      ]
-   }
+                  title: this.meta.meta_title,
+                  meta: [
+                     { name: 'description', vmid: 'description', content: this.meta.meta_description },
+                     { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
+                     { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
+                     { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
+                     { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
+                  ]
+               }
 
             }
         });
