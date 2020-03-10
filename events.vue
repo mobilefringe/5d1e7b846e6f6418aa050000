02282e4a4b2399419e7635de9e61dd531d33092d
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2020-03-09 21:33:26 -0400

    Kanata Central BIA 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -7,50 +7,22 @@
                     <h2 class="inside_subheading">Join us to celebrate and enjoy!</h2>
                 </div>
             </div>
-            <div v-if="events.length > 0">
-                <div class="custom_container col-12 col-md-12" style="padding: 0px" v-for="(item, index) in events">
-                <div class="row" style="width:100%">
+            <div class="custom_container" v-if="events.length > 0" v-for="(item, index) in events">
+                <div class="row">
                     <div class="col-12 col-sm-4 col-md-4 event_list_img">
-                    <img :src="item.image_url" class="promo_list_img" alt="" v-if="item.image_url">
+                        <img :src="item.image_url" class="promo_list_img" alt="" v-if="item.image_url">
                         <img :src="item.store.store_front_url_abs" class="promo_list_img" alt="" v-else>
                     </div>
                     <div class="col-12 col-sm-8 col-md-8">
-                    <div class="custom_right">
-                        <h3 class="description_text">{{ item.name }}</h3>
-                        <p class="top_temp_event_date" v-if="isMultiDay(item)">
-                            {{item.start_date | moment("MMMM D", timezone)}} - {{item.end_date | moment("MMMM D", timezone)}}
-                        </p>
-                        <p class="top_temp_event_date" v-else>{{ item.start_date | moment("MMMM D", timezone)}}</p>
-                        <div class="custom_desc">{{item.description}}</div>
-                        <router-link :to="{ name: 'eventDetails', params: { id: item.slug }}">Read More</router-link>
-                    </div>
-                </div>
-                </div>
-                </div>
-            </div>
-            <div class="row" v-else>
-                <div class="col-sm-12">
-                    <p>We don't currently have any events scheduled. To stay up to date, sign-up for our <a href="/newsletter">newsletter</a>, <a href="https://www.facebook.com/kanatacentralBIA/" target="_blank">follow us on Facebook</a>, or <a href="https://www.instagram.com/kanatacentralbia/" target="_blank">follow us on Instagram</a>.</p>
-                </div>
-            </div>
-            
-        </div>
-        <!--<div id="promotions_container" class="margin_25_across">
-            <h1 class="inside_heading">Exclusive Events at {{ property.name}}!</h1>
-            <h2 class="inside_subheading">Join us to celebrate and enjoy!</h2>
-            <div class="row" v-if="events.length > 0">
-                <div class="col-sm-6 col-md-4" v-for="(item, index) in events">
-                    <hr class="show_phone">
-                    <div class="promo_list_container text_center">
-                        <div class="promo_list_img_container">
-                            <img :src="item.store.store_front_url_abs" class="promo_list_img" alt="">
+                        <div class="custom_right">
+                            <h3 class="description_text">{{ item.name }}</h3>
+                            <p class="top_temp_event_date" v-if="isMultiDay(item)">
+                                {{item.start_date | moment("MMMM D", timezone)}} - {{item.end_date | moment("MMMM D", timezone)}}
+                            </p>
+                            <p class="top_temp_event_date" v-else>{{ item.start_date | moment("MMMM D", timezone)}}</p>
+                            <div class="custom_desc">{{item.description}}</div>
+                            <router-link :to="{ name: 'eventDetails', params: { id: item.slug }}">Read More</router-link>
                         </div>
-                        <h3 class="description_text">{{ item.name }}</h3>
-                        <p class="top_temp_event_date" v-if="isMultiDay(item)">
-                            {{item.start_date | moment("MMMM D", timezone)}} - {{item.end_date | moment("MMMM D", timezone)}}
-                        </p>
-                        <p class="top_promo_date" v-else>{{ item.start_date | moment("MMMM D", timezone)}}</p>
-                        <router-link :to="{ name: 'eventDetails', params: { id: item.slug }}" class="animated_btn text_center">View Event Details</router-link>
                     </div>
                 </div>
             </div>
@@ -59,7 +31,32 @@
                     <p>We don't currently have any events scheduled. To stay up to date, sign-up for our <a href="/newsletter">newsletter</a>, <a href="https://www.facebook.com/kanatacentralBIA/" target="_blank">follow us on Facebook</a>, or <a href="https://www.instagram.com/kanatacentralbia/" target="_blank">follow us on Instagram</a>.</p>
                 </div>
             </div>
-        </div>-->
+        </div>
+        <!--<div id="promotions_container" class="margin_25_across">-->
+        <!--    <h1 class="inside_heading">Exclusive Events at {{ property.name}}!</h1>-->
+        <!--    <h2 class="inside_subheading">Join us to celebrate and enjoy!</h2>-->
+        <!--    <div class="row" v-if="events.length > 0">-->
+        <!--        <div class="col-sm-6 col-md-4" v-for="(item, index) in events">-->
+        <!--            <hr class="show_phone">-->
+        <!--            <div class="promo_list_container text_center">-->
+        <!--                <div class="promo_list_img_container">-->
+        <!--                    <img :src="item.store.store_front_url_abs" class="promo_list_img" alt="">-->
+        <!--                </div>-->
+        <!--                <h3 class="description_text">{{ item.name }}</h3>-->
+        <!--                <p class="top_temp_event_date" v-if="isMultiDay(item)">-->
+        <!--                    {{item.start_date | moment("MMMM D", timezone)}} - {{item.end_date | moment("MMMM D", timezone)}}-->
+        <!--                </p>-->
+        <!--                <p class="top_promo_date" v-else>{{ item.start_date | moment("MMMM D", timezone)}}</p>-->
+        <!--                <router-link :to="{ name: 'eventDetails', params: { id: item.slug }}" class="animated_btn text_center">View Event Details</router-link>-->
+        <!--            </div>-->
+        <!--        </div>-->
+        <!--    </div>-->
+        <!--    <div class="row" v-else>-->
+        <!--        <div class="col-sm-12">-->
+        <!--            <p>We don't currently have any events scheduled. To stay up to date, sign-up for our <a href="/newsletter">newsletter</a>, <a href="https://www.facebook.com/kanatacentralBIA/" target="_blank">follow us on Facebook</a>, or <a href="https://www.instagram.com/kanatacentralbia/" target="_blank">follow us on Instagram</a>.</p>-->
+        <!--        </div>-->
+        <!--    </div>-->
+        <!--</div>-->
     </div>
 </template>
 
