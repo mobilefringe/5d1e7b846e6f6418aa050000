f10d5a3ad2ccdfb0a3728a13f027dda84a39d7eb
mithun@mobilefringe.com @ CodeCloud <mithun@mobilefringe.com @ CodeCloud>
2020-04-13 11:46:52 -0400

    Kanata Central BIA 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -7,7 +7,6 @@
                 <div class="custom_container col-12 col-md-12" style="padding: 0px" v-for="(item, index) in events">
                 <div class="row custom_height" style="width:100%">
                     <div class="col-12 col-sm-4 col-md-4 event_list_img">
-                    ITEM URL {{item.store.store_front_url_abs}}
                     <img :src="item.image_url" class="promo_list_img" alt="" v-if="item.image_url">
                     <img :src="item.store.store_front_url_abs" class="promo_list_img" alt="" v-else>
                     </div>
